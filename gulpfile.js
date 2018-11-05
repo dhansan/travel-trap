5406a95efec46b2c1678be9b62714dd5c49c0042
Dhanu Satriyo <dhanu.satriyo@gmail.com>
2018-11-05 14:34:31 +0700

    npm install gulp, gulp-watch, gulp-postcss, autoprefixer, postcss-simple-vars, postcss-nested

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+//////////////
+// Required //
+//////////////
+
+var gulp 		      = require('gulp'),
+    watch         = require('gulp-watch'),
+    postcss       = require('gulp-postcss'),
+    autoprefixer  = require('autoprefixer'),
+    cssvars       = require('postcss-simple-vars'),
+    nested       = require('postcss-nested');
+
+gulp.task('default', function() {
+  console.log("test berhasil");
+});
+//////////////////
+// Styles  Task //
+//////////////////
+
+gulp.task('html', function() {
+  console.log("test2 yeaah");
+});
+
+gulp.task('styles', function() {
+  gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+//////////////////
+// Watch   Task //
+//////////////////
+gulp.task('watch', function() {
+  watch('./app/index.html', function() {
+    gulp.start('html');
+  });
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('styles');
+  });
+});
